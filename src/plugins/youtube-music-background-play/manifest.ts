9c99a3330685c56906d92595dee1f1d37cdcc130
minhtienth15 <minhtienth15@gmail.com>
2026-09-04 09:11:35 +0700

    Update manifest.ts

--- src/plugins/youtube-music-background-play/manifest.ts
+++ src/plugins/youtube-music-background-play/manifest.ts
@@ -2,10 +2,10 @@ import type { MonkeyUserScript } from 'vite-plugin-monkey'
 
 export function manifest(): MonkeyUserScript {
   return {
-    name: 'YouTube Music Background Play',
+    name: 'YouTube Background Play',
     namespace: 'https://rxliuli.com',
-    description: 'Keep YouTube Music playing in background',
-    match: ['https://music.youtube.com/*'],
+    description: 'Keep YouTube playing in background',
+    match: ['https://*.youtube.com/*'],
     'run-at': 'document-start',
     sandbox: 'DOM',
     grant: 'none',
